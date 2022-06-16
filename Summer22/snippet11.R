82b39bf40b50283bd6fe64381097424973e2e1d6
rsparapa <rsparapa@mcw.edu>
2022-06-16 12:27:43 -0500

    add

---
+++ Summer22/snippet11.R
@@ -0,0 +1,13 @@
+## snippet11.R
+zombies=dbGetQuery(db,
+                   "select distinct a.patient_hash, 
+                    a.death_date_shifted,
+                    a.vital_status_source
+                    from fh_hb_demographics_jupyter a
+                    inner join fh_hb_vitals_jupyter b 
+                    on a.patient_hash=b.patient_hash 
+                    AND a.death_date_shifted<
+                    b.measure_date_shifted
+                    order by patient_hash")
+table(zombies$vital_status_source)
+print(zombies)
